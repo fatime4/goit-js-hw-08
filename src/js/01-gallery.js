099899b076e4265773ee6fb3d4cc17ca40234abf
fatime4 <102423500+fatime4@users.noreply.github.com>
2022-07-17 13:26:39 +0300

    task 1 hw-08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+const galleryBoxEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const galleryItemsEl = galleryItems.map(el => {
+  return `
+  <a class="gallery__link" href="${el.original}">
+    <img
+      class="gallery__image"
+      src="${el.preview}"
+      data-source="${el.original}"
+      alt="${el.description}"
+    />
+  </a>`;
+});
+
+galleryBoxEl.insertAdjacentHTML('afterbegin', galleryItemsEl.join(''));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (data) {};
+
+player.on('play', onPlay);
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
