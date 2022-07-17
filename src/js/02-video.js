18dd386c3de72af4732f7bd127f6ae98164cd43d
fatime4 <102423500+fatime4@users.noreply.github.com>
2022-07-17 17:49:39 +0300

    task 3 hw-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,21 @@
 import Player from '@vimeo/player';
+import _ from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const storageKey = 'videoplayer-current-time';
 
-const onPlay = function (data) {};
+const saveCurrentTime = function (data) {
+  const timeUpdateSec = localStorage.setItem(
+    storageKey,
+    JSON.stringify(data.seconds)
+  );
+};
 
-player.on('play', onPlay);
+player.on('timeupdate', _.throttle(saveCurrentTime, 1000));
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+const videoCurrentTime = localStorage.getItem(storageKey);
+
+if (videoCurrentTime) {
+  player.setCurrentTime(JSON.parse(videoCurrentTime));
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import localStorageApi from './localstorage';
 
+const formEl = document.querySelector('.feedback-form');
+// Визначаємо перемінні
+const storageKey = 'feedback-form-state';
+const userData = {};
+// При заповненні поля форми
+const formFieldsFill = () => {
+  const userDatafromLS = localStorageApi.load('formData');
+
+  if (userDatafromLS === undefined) {
+    return;
+  }
+
+  const elem = formEl.elements;
+
+  for (const key in userDatafromLS) {
+    if (userDatafromLS.hasOwnProperty(key)) {
+      elem[key].value = userDatafromLS[key];
+    }
+  }
+};
+// Додаємо дані з інпутів в local storage
+const onFormElInput = event => {
+  const target = event.target;
+
+  const formElValue = target.value;
+  const formElName = target.name;
+
+  userData[formElName] = formElValue;
+
+  localStorageApi.save('formData', userData);
+};
+
+const onFormSubmit = event => {
+  event.preventDefault();
+  // Очищуємо local storage
+  localStorageApi.remove('formData');
+  // Reset для очищення полів форми
+  event.currentTarget.reset();
+};
+// Додаємо слухачів
+formEl.addEventListener('input', onFormElInput);
+formEl.addEventListener('submit', onFormSubmit);
+
+formFieldsFill();
